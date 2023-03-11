ccf1c31f13002211ee06a6c3f7d9343d15597d2e
KranthiKumarNagothu <97676758+KranthiKumarNagothu@users.noreply.github.com>
2023-03-11 11:44:01 +0530

    Update DistinctValueMapper.java

--- DistinctValueMapper.java
+++ DistinctValueMapper.java
@@ -20,3 +20,32 @@ public class DistinctValueMapper extends Mapper<LongWritable, Text, Text, IntWri
 	    }
 	}
 	}
+
+
+
+package com.dv;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class DistinctValueReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException,
+	InterruptedException {
+		int wordCount = 0;
+		for (IntWritable value : values) {
+			wordCount += value.get();
+		}
+		if(wordCount == 1)
+			context.write(key, new IntWritable(wordCount));
+	
+}
+}
+
+
+
+
+
